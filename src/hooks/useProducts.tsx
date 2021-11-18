0b026bc62e8bbc46f89cc861b9756789e0b9525c
luizsmatos <gusstavopdm@gmail.com>
2021-11-18 14:53:52 -0300

    feature: Estrutura ContextAPI, useProducts

---
+++ src/hooks/useProducts.tsx
@@ -0,0 +1,19 @@
+import React, { useContext, ReactNode, createContext } from 'react';
+
+interface ProductsProviderProps {
+  children: ReactNode;
+}
+
+const ProductsContext = createContext({});
+
+export const ProductsProvider = ({ children }: ProductsProviderProps) => {
+  return (
+    <ProductsContext.Provider value="">{children}</ProductsContext.Provider>
+  );
+};
+
+export function useProducts() {
+  const context = useContext(ProductsContext);
+
+  return context;
+}
